a6aa232ade72c862101b75da016a20a9bb7daac2
Jack Ngai <jackngai@gmail.com>
2017-01-03 12:30:23 -0500

    docs: Added "To Do List"
    Added a list of items that needs to be added to the app.

---
+++ ForNYers/ToDo.swift
@@ -0,0 +1,19 @@
+//
+//  ToDo.swift
+//  ForNYers
+//
+//  Created by Jack Ngai on 1/3/17.
+//  Copyright © 2017 Jack Ngai. All rights reserved.
+//
+
+/*
+
+ – Dynamically adjust font size for Settings view
+ - Correct issue with Metrocard Refill Calculator as penny values are not accepted 
+ - In Metrocard Refill Calculator, add feature to use bonus to calculate cost to fill card without leaving balance
+ - In Tip Calculator, add label to show 17.75% is double tip
+ - In Settings view, add pop-up message to explain stride settings
+ - In News view, add link to newsapi.org
+ - Add credits to icons
+ 
+*/
